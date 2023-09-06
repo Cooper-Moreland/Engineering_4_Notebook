ee1d0c829e90a7fe06220d568ba7baca257dcf60
Cooper_Moreland <wmorela54@charlottesvilleschools.org>
2023-09-06 13:38:50 -0400

    launch pad part 1

--- raspberry-pi/2-1 countdown.py
+++ raspberry-pi/2-1 countdown.py
@@ -3,6 +3,6 @@ import time
 import board
 
 for x in range(10, 0, -1): # in the range from 10 to 0 going down by 1
-    print(x)
+    print(x) # print the variable
     time.sleep(1.0) # delay of 1 sec
-print("Liftoff!")
+print("LIFTOFF!")
