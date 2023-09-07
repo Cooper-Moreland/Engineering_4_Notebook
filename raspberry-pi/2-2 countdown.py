63995e3fda0bdf876882a2aaeba9f3008c230475
Cooper_Moreland <wmorela54@charlottesvilleschools.org>
2023-09-07 13:44:57 -0400

    launch pad part 2

--- raspberry-pi/2-2 countdown.py
+++ raspberry-pi/2-2 countdown.py
@@ -3,13 +3,17 @@ import time
 import board
 import digitalio
 
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
+led1 = digitalio.DigitalInOut(board.GP16)
+led1.direction = digitalio.Direction.OUTPUT
+led2 = digitalio.DigitalInOut(board.GP17)
+led2.direction = digitalio.Direction.OUTPUT # red and green led output location
 
 for x in range(10, 0, -1): # in the range from 10 to 0 going down by 1
     print(x) # print the variable
-    led.value = True
+    led1.value = True
     time.sleep(0.5)
-    led.value = False
-    time.sleep(0.5)
+    led1.value = False
+    time.sleep(0.5) # blink red led
 print("LIFTOFF!")
+led2.value = True
+time.sleep(5.0) # green led on for 5 secs
