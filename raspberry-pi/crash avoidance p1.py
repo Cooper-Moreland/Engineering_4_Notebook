1bbeb413cc99e5ef9f405180926ee727515229b6
Cooper_Moreland <wmorela54@charlottesvilleschools.org>
2023-09-15 13:32:25 -0400

    crash avoidance p1

--- raspberry-pi/crash avoidance p1.py
+++ raspberry-pi/crash avoidance p1.py
@@ -10,6 +10,6 @@ i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c) # set up for variables and pin locations
 
 while True:
-    accleration = mpu.acceleration # new var
-    print(f"X: {acceleration[0]} m/s^2 Y: {acceleration[1]} m/s^2 Z: {acceleration[1]} m/s^2") # print x, y, and z values
-    time.sleep(0.25) # debounce
+    acc = mpu.acceleration # new var
+    print(f"X: {acc[0]} m/s^2 Y: {acc[1]} m/s^2 Z: {acc[1]} m/s^2") # print x, y, and z values
+    time.sleep(0.75) # debounce
