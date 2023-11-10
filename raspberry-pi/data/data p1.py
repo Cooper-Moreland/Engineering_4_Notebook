9023906d33ca87f737ad88b8b2df8e164f0f282a
Cooper_Moreland <wmorela54@charlottesvilleschools.org>
2023-11-10 13:59:03 -0500

    data p1

--- raspberry-pi/data/data p1.py
+++ raspberry-pi/data/data p1.py
@@ -6,7 +6,7 @@ import board
 import time
 import digitalio
 import displayio
-import storage
+import storage # imports
 
 displayio.release_displays() # this needs to be first in the code
 
@@ -20,23 +20,23 @@ i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c) # set up for variables and pin locations
 
 while True: 
-    acc = mpu.acceleration
-    print(f"X: {acc[0]}m/s² Y: {acc[1]}m/s² Z: {acc[2]}m/s²")
-    time.sleep(0.5)
-    led1.value = False
+    acc = mpu.acceleration # new var
+    print(f"X: {acc[0]}m/s² Y: {acc[1]}m/s² Z: {acc[2]}m/s²") # f string for accel of the breadboard
+    time.sleep(0.25)
+    led1.value = False # default led is off
     if abs(acc[2]) < 2:
-        led1.value = True
-    if not storage.getmount("/").readonly:
-        # we can write to the filesystem
+        led1.value = True # when tilted sideway turn the led on 
+    if not storage.getmount("/").readonly: # call back to the boot.py file
         with open("/data.csv", "a") as datalog:
-            written_string = f"{time.monotonic()},{acc[0]},{acc[1]},{acc[2]},{tilt_led.value}\n"
-            datalog.write(written_string)
-            datalog.flush()
-            # blink onboard LED to indicate activity
+            time_elapsed = time.monotonic()
+            csv_string = f"{time_elapsed},{acc[0]},{acc[1]},{acc[2]},{led1.value}\n"
+            # f string showing time, acc, and whether or not it's tilted
+            datalog.write(csv_string)
             led2.value = True
-            time.sleep(0.25)
+            time.sleep(0.1)
             led2.value = False
+            time.sleep(0.1) # blink led to show switch between modes
+            datalog.flush() # record stuff to the datalog
             time.sleep(0.25)
     else:
-        # sleep anyways
         time.sleep(0.25)
