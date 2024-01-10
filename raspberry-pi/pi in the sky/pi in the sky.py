423b2afa364e505655a12170e63ada94b7e1ef9c
Cooper_Moreland <wmorela54@charlottesvilleschools.org>
2024-01-10 13:14:41 -0500

    v1 of project code

--- raspberry-pi/pi in the sky/pi in the sky.py
+++ raspberry-pi/pi in the sky/pi in the sky.py
@@ -10,7 +10,7 @@ import storage # imports
 
 displayio.release_displays() # this needs to be first in the code
 
-led = digitalio.DigitalInOut(board.LED)
+led = digitalio.DigitalInOut(board.GP16)
 led.direction = digitalio.Direction.OUTPUT
 sda_pin = board.GP14
 scl_pin = board.GP15
@@ -19,6 +19,7 @@ sensor = adafruit_mpl3115a2.MPL3115A2(i2c) # set up for variables and pin locati
 
 sensor.sealevel_pressure = 1011.42 
 # standard pressure is 1013.25 hPa set this to however much gives the initial point 0 height
+# higher pressure = higher recorded height
 
 while True: 
     altitude = sensor.altitude # new var
@@ -27,10 +28,10 @@ while True:
     time.sleep(1.0)
     if not storage.getmount("/").readonly: # call back to the boot.py file
         with open("/data.csv", "a") as datalog:
-            time_elapsed = time.monotonic()
+            time_elapsed = time.monotonic() # set time to run normally
             csv_string = f"{time_elapsed},{altitude}\n"
             # f string showing time and altitude
-            datalog.write(csv_string)
+            datalog.write(csv_string) # record f string in separate columns in microsoft excel
             led.value = True
             time.sleep(0.1)
             led.value = False
